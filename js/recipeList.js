3890b0735fc387d4c95604aee083577b2383627e
JacGreening <greeningjacklyn@gmail.com>
2025-04-21 15:01:42 -0500

    calendar js update, event added to calendar, php to pull recipes (uncomplete), working on autofilling from recipe database and including link

--- js/recipeList.js
+++ js/recipeList.js
@@ -1,26 +1,73 @@
 const recipeNames =
  [
    {
-      name: "The Perfect Grilled Cheese",
-    link: "https://www.foodnetwork.com/recipes/alton-brown/grilled-cheese-recipe-1942462",
+   name: "The Perfect Grilled Cheese",
+    link: fetchRecipies("The Perfect Grilled Cheese"),
    }, 
    {
       name: "The Best Chocolate Chip Cookie Recipe Ever",
-      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+      link: "../recipePull.php? name=The Best Chocolate Chip Cookie Recipe Ever",
     },
 
     {
       name: "Greek Quesadillas",
+      link: "../recipePull.php name=Greek Quesadillas",
+   },
+
+   {
+      name: "Burrito Bowls",
       link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
    },
 
    {
-      name: "Cheesebureger Pizza",
+      name: "Creamy Brocolli Pasta",
+      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+   },
+
+   {
+      name: "Quinoa Salad",
+      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+   },
+
+   {
+      name: "Pizza Burgers",
+      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+   },
+
+   {
+      name: "Overnight Oats",
+      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+   },
+
+   {
+      name: "Mediterranean Sheet Pan Vegetabes",
       link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
    },
 
    {
       name: "Mushroom Pasta",
       link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
-   }
+   },
+
+   {
+      name: "Egg Quiche",
+      link: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chocolate-chip-cookie-recipe-ever-3362950",
+   },
 ];
+
+
+async function fetchRecipies(recipeName) {
+ try {
+   const response = await fetch('../../recipePull.php', recipeName);
+   if (!response.ok) 
+      {
+      throw new Error('Failed to fetch recipes');
+   }
+   const recipe = await response.json();
+   return recipeLink;
+ }  
+ catch (error) {
+   console.error('Error fetching recipes:', error);
+   return null;
+ }
+}
--- recipePull.php
+++ recipePull.php
@@ -1,39 +1,25 @@
 <?php
 $servername="localhost";
-$usernaem="root";
+$username="root";
 $password="";
-$database ="recipe";
+$database ="recipes";
 
 //create connection
-$conn = new mysqli($servername, $usernaem, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 //catch connection errors
 if($conn->connect_error)
 {
-    die("Connection failed: " . $conn-connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM recipe";
-$result = $conn->query($query);
 
-if (result->num_rows >0)
+function getRecipe($name)
 {
-    while($row = $result->fetch_assoc())
-    {
-        echo "Recipe Name: " . $row["recipe_name"]. "<br>";
-           $row["recipe_servings"]. "<br>";
-           $row["recipe_ingredients"]. "<br>";
-           $row["recipe_notes"]. "<br>";
-           $row["recipe_rating"]. "<br>";
-           $row["recipe_time"]. "<br>";
-           $row["recipe_author"]. "<br>";
-           $row["recipe_link"]. "<br>";
-    }
-}
-
-else 
-{
-    echo "0 results";
+    global $conn;
+    $query = "SELECT link FROM recipe WHERE name = 'recipeName'" ;
+    $result = $conn->query($query);
+    return $result->fetch_assoc()['link'];
 }
 
 $conn->close();
